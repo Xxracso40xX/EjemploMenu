dafb3a472e6d08e023f950d5adc32e49c40ad7de
Óscar <Xxracso40xX@gmail.com>
2019-01-08 10:32:37 +0100

    Initial commit

---
+++ app/src/main/java/com/example/pc/ejemplomenu/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.pc.ejemplomenu;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Llamar por teléfono:
+            case R.id.opcion1:
+                Intent i1 = new Intent(Intent.ACTION_DIAL);
+                startActivity(i1);
+                break;
+            // Acceder a una página Web:
+            case R.id.opcion2:
+                Intent i2 = new Intent(Intent.ACTION_VIEW);
+                i2.setData(Uri.parse("http://www.facebook.com"));
+                startActivity(i2);
+                break;
+            // Ejecutar una Activity de la misma aplicación:
+            case R.id.opcion3:
+                Intent i3 = new Intent(this, AcercaDe.class);
+                startActivity(i3);
+                break;
+        }
+        return true;
+    }
+}
